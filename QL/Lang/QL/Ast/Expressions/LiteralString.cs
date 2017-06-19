5d510677e683726c8380a31f13b63e96f88f65ff
Floris den Heijer <floris@denheijer.com>
2017-06-20 00:59:35 +0200

    Initial commit with AST mostly complete

---
+++ QL/Lang/QL/Ast/Expressions/LiteralString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL.Lang.QL.Ast.Expressions
+{
+    public class LiteralString : Literal
+    {
+        public string Value { get; set; }
+    }
+}
